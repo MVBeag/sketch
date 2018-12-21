c58230e1dab27c3c7939dd64669eafdd557bf783
voegelmichael <voegelmichael@aol.com>
2018-12-21 22:51:52 +0100

    clean and synchro

--- main.cpp
+++ main.cpp
@@ -3,20 +3,9 @@
 #include <QCoreApplication>
 #include "eventfilter.h"
 
-//#include "/home/voegelm/NG2018/dey/2.4-r1/ccimx6ulsbc/dey-image-qt-fb/sysroots/cortexa7hf-neon-dey-linux-gnueabi/usr/include/tslib.h"
-//#include <tslib.h>
-
 int main(int argc, char *argv[])
 {
-    //::setAttribute(Qt::AA_EnableHighDpiScaling);
-    //QCoreApplication::setAttribute( Qt::WA_AcceptTouchEvents );
-
     QGuiApplication app(argc, argv);
-//  app.setAttribute(Qt::WA_AcceptTouchEvents);
-   // ts_calibrate;
-   //evtfilter evt;
-    //app.installEventFilter(&evt);
-
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
